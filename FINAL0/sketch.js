d6f4938720ede6e2cab405c372136e32305ad1bf
kevinhb92 <leeh965@ihyeonbeomuiMBP.wireless.newschool.edu>
2015-12-18 17:28:24 -0500

    final0

---
+++ FINAL0/sketch.js
@@ -0,0 +1,78 @@
+// list the illustration that i am going to use
+var backhandImg;
+var backgroundImg;
+var bulletImg;
+var fingerImg;
+var fronthandImg;
+var lowergunImg;
+var uppergunImg;
+var reloadSound;
+var fireSound;
+var reloadImg;
+// touch
+//place the bullet
+var position = 400;
+
+//preload the elements
+function preload() {
+	backgroundImg = loadImage("images_1/background.png");
+	backhandImg = loadImage("images_1/backhand.png");
+	bulletImg = loadImage("images_1/bullet.png");
+	fingerImg = loadImage("images_1/finger.png");
+	fronthandImg = loadImage("images_1/fronthand.png");
+	lowergunImg = loadImage("images_1/lowergun.png");
+	uppergunImg = loadImage("images_1/uppergun.png");
+	reloadImg = loadImage("images_1/RELOAD.png");
+	reloadSound = loadSound('images_1/reload.mp3');
+	fireSound = loadSound('images_1/fire.mp3');
+}
+
+//draw the canvas
+function setup() {
+	createCanvas(640, 480);
+}
+
+//start drawing
+function draw() {
+	image(backgroundImg, 0, 0);
+	image(backhandImg, 190, 250);
+	image(lowergunImg, 40, 148);
+	image(reloadImg, 400, 280);
+
+	//give a attribute to bullet
+	position = position + 3;
+	if (mouseIsPressed) {
+		position = 400;
+	}
+	if (mouseX > 205) {
+		position = 400;
+	}
+	image(bulletImg, position, 130);
+
+	//give a attribute to the upper gun
+	if ((mouseX - 140) <= 45) {
+		mouseX = 185;
+	}
+	if ((mouseX - 140) >= 90) {
+		mouseX = 230;
+	}
+	if (mouseIsPressed) {
+		image(uppergunImg, mouseX - 140, 91);
+	} else {
+		image(uppergunImg, 83, 91);
+	}
+
+	//give a attribute to the finger
+	if (mouseX <= 190) {
+		mouseX = 190;
+	}
+	if (mouseX >= 305) {
+		mouseX = 295;
+	}
+	if (mouseIsPressed) {
+		image(fingerImg, mouseX, 200);
+	} else {
+		image(fingerImg, 225, 200);
+	}
+	image(fronthandImg, 0, 200);
+}
